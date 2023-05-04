ef5f23aa5b7b26d0244693445c9bf2e183b03f11
Baptiste Dupache <baptistedupache@gmail.com>
2023-05-04 13:58:11 +0200

    tttt

--- lua/autorun/client/cl_scoreboard.lua
+++ lua/autorun/client/cl_scoreboard.lua
@@ -22,29 +22,44 @@ local function ToggleScoreboard(toggle)
         MonAddonScoreboard:MakePopup() -- comment il va apparaitre
         MonAddonScoreboard:ShowCloseButton(false) -- pour enlever le btn en haut
         MonAddonScoreboard:SetDraggable(false) -- empeche de bouger la Dframe
-        MonAddonScoreboard.Paint = function(self,w,h)
-            surface.SetDrawColor(0,0,0,230)
-            surface.DrawRect(0,0,w,h)
-            draw.SimpleText("Scoreboard", "ROBOTO22", w / 2, h * .015, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+        MonAddonScoreboard.Paint = function(self,w,h) -- permet de créer la box
+            surface.SetDrawColor(0,0,0,150) -- met la couleur de la box
+            surface.DrawRect(0,0,w,h) -- créer la taille de la box
+            draw.SimpleText("Liste des joueurs", "ROBOTO22", w / 2, h * .015, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER) -- permet d'avoir du text 
         end
-        local scroll = vgui.Create("DScrollPanel", MonAddonScoreboard)
-        scroll:SetPos(0, MonAddonScoreboard:GetTall() * .03)
-        scroll:SetSize(MonAddonScoreboard:GetWide(), MonAddonScoreboard:GetTall() * .5)
-        local ypos = 0
-        for k, v in pairs(player.GetAll()) do
-            local playerPanel = vgui.Create("DPanel", scroll)
-            playerPanel:SetPos(0, ypos)
-            playerPanel:SetSize(MonAddonScoreboard:GetWide(), MonAddonScoreboard:GetTall() * .05)
-            local name = v:Name()
-            local ping = v:Ping()
-            playerPanel.Paint = function(self,w,h)
-                if IsValid(v) then
-                    surface.SetDrawColor(0, 0, 0, 200)
-                    surface.DrawRect(0,0,w,h)
-                    draw.SimpleText(name .. " Ping : " .. ping, "ROBOTO18", w / 2, h / 2, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+        local scroll = vgui.Create("DScrollPanel", MonAddonScoreboard) -- création du scroll
+        scroll:SetPos(0, MonAddonScoreboard:GetTall() * .03) -- règle la taille de l'espace pour le texte en haut du scoreboard
+        scroll:SetSize(MonAddonScoreboard:GetWide(), MonAddonScoreboard:GetTall() * 0.95) -- la taille de la liste si il est = à 100 on peut pourra pas voir tout les joueurs
+        local sbar = scroll:GetVBar()
+        function sbar:Paint(w, h)
+            draw.RoundedBox(0, 0, 0, w, h, Color(55, 55, 55)) -- bg de la barre de scroll
+        end
+        function sbar.btnUp:Paint(w, h)
+            draw.RoundedBox(0, 0, 0, w, h, Color(0, 0, 0)) -- btn up
+        end
+        function sbar.btnDown:Paint(w, h)
+            draw.RoundedBox(0, 0, 0, w, h, Color(0, 0, 0)) -- btn down
+        end
+        function sbar.btnGrip:Paint(w, h)
+            draw.RoundedBox(5, 0, 0, w, h, Color(150, 150, 150)) -- barre
+        end
+        local ypos = 0 -- la position 
+        for k, v in pairs(player.GetAll()) do -- permet de vérifier si le joueurs regarde toujours le garde à jours
+            local playerPanel = vgui.Create("DPanel", scroll) -- permet de créer chaque case du scoreboard
+            playerPanel:SetPos(0, ypos) -- met en place la position de la case
+            playerPanel:SetSize(MonAddonScoreboard:GetWide(), MonAddonScoreboard:GetTall() * .05) -- permet de régler la taille de chaque case
+            local name = v:Name() -- variable qui stock le nom du joueurs
+            local ping = v:Ping() -- variable qui stock le ping du joueurs
+            playerPanel.Paint = function(self,w,h) -- permet de créer la box
+                if IsValid(v) then -- vérifie si il y a des choses à afficher en plus ou en moins
+                    surface.SetDrawColor(0, 0, 0, 100) -- met la couleur de la box
+                    surface.DrawRect(0,0,w,h) -- créer la taille de la box
+                    draw.SimpleText(name, "ROBOTO18", w / 6, h / 2, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER) -- le texte pour le nom
+                    draw.SimpleText("Ping : " .. ping, "ROBOTO18", w / 1.2, h / 2, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER) -- le texte pour le ping
                 end
             end
-            ypos = ypos + playerPanel:GetTall() * 1.1
+            ypos = ypos + playerPanel:GetTall() * 1.1 -- il écarte pour empiler et ne pas stacker sur place les joueurs
+/*             local playerInfo = vgui.Create(D, parent=nil, name=nil) */
         end
     else
         if IsValid(MonAddonScoreboard) then -- si il arrete d'appuier sur son "toggle"
